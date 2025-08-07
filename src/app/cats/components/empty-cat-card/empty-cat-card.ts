0569f8baa277598cbf140bffa5871efecbfd2241
Gummiees <xavicaromiro@gmail.com>
2025-08-07 16:58:22 +0200

    refactor(cats): multiple small improvements

--- src/app/cats/components/empty-cat-card/empty-cat-card.ts
+++ src/app/cats/components/empty-cat-card/empty-cat-card.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, output } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 @Component({
@@ -7,4 +7,6 @@ import { CommonModule } from "@angular/common";
   standalone: true,
   imports: [CommonModule],
 })
-export class EmptyCatCard {}
+export class EmptyCatCard {
+  onClick = output<void>();
+}
