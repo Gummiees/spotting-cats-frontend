b92fe761fa97a3806837226238d24c6a38d2e3fa
Gummiees <xavicaromiro@gmail.com>
2025-08-07 13:02:10 +0200

    refactor(env): ignore environment files and remove them

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: "API_URL",
-  maptilerApiKey: "MAPTILER_API_KEY",
-};
--- src/environments/environment.staging.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: "API_URL",
-  maptilerApiKey: "MAPTILER_API_KEY",
-};
